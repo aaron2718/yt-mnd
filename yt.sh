8834dff9e9e28bc17fef307fc977a7bdb1d0356a
Aaron Bocksberger <aaron-b1@gmx.de>
2025-10-12 18:03:33 +0200

    Added a option to get more results

--- yt.sh
+++ yt.sh
@@ -4,33 +4,35 @@ instance="https://inv.nadeko.net"
 
 while true; do
 	clear
-
 	read -p 'Search: ' query
 	[ -z "$query" ] && clear && break
-
 	clear
 	echo "loading results ..."
+
+	if [ -z "$1" ]; then
+		p=1
+	else
+		p=$1
+	fi		
 	search_query=$(echo "$query" | sed 's/ /+/g')
-	i=1
+	pages=""
 
-	while [ "$i" = 1 ]; do
-		results=$(curl -s "$instance/search?q=$search_query" | \
-		grep '<p dir="auto"' | \
-		sed -n 's|.*watch?v=\([^"]*\).*<p dir="auto">\([^<]*\).*|https://www.youtube.com/watch?v=\1 \2|p')
+	for i in $(seq 1 "$p"); do
+		page=$(curl -s "$instance/search?q=$search_query&page=$i")
+		pages="$pages\n$page"
+	done
 
-		display=$(echo "$results" | awk '{$1=""; print}' | nl -n ln)
+	results=$(echo "$pages" | grep '<p dir="auto"' | \
+	sed -n 's|.*watch?v=\([^"]*\).*<p dir="auto">\([^<]*\).*|https://www.youtube.com/watch?v=\1 \2|p')
+	display=$(echo "$results" | awk '{$1=""; print}' | nl -n ln)
 
+	while true; do
 		selection=$(echo "back\n$display" | fzf --multi --reverse)
 
-		if [ "$selection" = "back" ]; then
-			i=0
-			continue
-		fi
+		[ "$selection" = "back" ] && break
 
 		number=$(echo "$selection" | awk '{print $1}')
-
 		all_url=$(echo "$results" | awk '{print $1}' | nl -n ln)
-
 		url=$(echo "$all_url" | grep -e "^$number\s" | awk '{print $2}')
 
 		clear
