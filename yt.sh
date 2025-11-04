77d25d48814514aebfca2d5dbeebb5d8307bc160
Aaron Bocksberger <aaron-b1@gmx.de>
2025-11-04 19:47:47 +0100

    Add more information to videos

--- yt.sh
+++ yt.sh
@@ -22,22 +22,45 @@ while true; do
 		pages="$pages\n$page"
 	done
 
-	results=$(echo "$pages" | grep '<p dir="auto"' | \
-	sed -n 's|.*watch?v=\([^"]*\).*<p dir="auto">\([^<]*\).*|https://www.youtube.com/watch?v=\1 \2|p')
-	display=$(echo "$results" | awk '{$1=""; print}' | nl -n ln)
+	length=$(echo "$pages" | grep "length" | grep ":" | sed -n 's|.*class="length">\([^<]*\).*|\1|p')
+	echo "$length" > /tmp/length
+
+	pub=$(echo "$pages" | grep "video-data" | grep -v " views" | \
+	sed -n 's|.*dir="auto">Shared \([^<]*\).*|\1|p' | sed "s/ ago//")
+	echo "$pub" > /tmp/pub
+
+	title=$(echo "$pages" | grep '<p dir="auto"' | grep 'href="/watch' | \
+	sed -n 's|.*<p dir="auto">\([^<]*\).*|\1|p')
+	echo "$title" > /tmp/title
+
+	channel=$(echo "$pages" | grep "channel-name")
+	line=$(echo "$channel" | nl -n ln | grep "@" | awk '{print $1}')
+	line_pre=$(expr $line - 1)
+	channel=$(echo "$channel" | sed "${line_pre},${line}d" | sed "s/.*>//" | grep -v '^[[:space:]]*$')
+	echo "$channel" > /tmp/channel
+	
+	views=$(echo "$pages" | grep " views" | sed -n 's|.*dir="auto">\([^<]*\).*|\1|p')
+	echo "$views" > /tmp/views
+
+	url=$(echo "$pages" | grep '<p dir="auto"' | grep 'href="/watch' | \
+	sed -n 's|.*watch?v=\([^"]*\).*|https://www.youtube.com/watch?v=\1|p' | nl -n ln)
+	echo "$url" > /tmp/url
+
+	display=$(paste /tmp/length /tmp/pub /tmp/views /tmp/channel /tmp/title | \
+	nl -n ln | sed "s/\t/\^/g" | sed "s/    //" | column -t -s "^" -o "  ")
 
 	while true; do
 		selection=$(echo "back\n$display" | fzf --multi --reverse)
 
 		[ "$selection" = "back" ] && break
+		[ "$selection" = "" ] && break
 
 		number=$(echo "$selection" | awk '{print $1}')
-		all_url=$(echo "$results" | awk '{print $1}' | nl -n ln)
-		url=$(echo "$all_url" | grep -e "^$number\s" | awk '{print $2}')
+		url_sel=$(echo "$url" | grep -e "^$number\s" | awk '{print $2}')
 
 		clear
 		echo "loading video ..."
-		mpv -fs $url
+		mpv -fs $url_sel
 	done
 done
 
